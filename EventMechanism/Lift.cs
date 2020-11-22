fbc420ceaae817688b066ad49b151018b6fdc027
TaneriMan <56047304+TaneriMan@users.noreply.github.com>
2020-11-22 16:46:54 +0300

    First Upload

---
+++ EventMechanism/Lift.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace EventMechanism
+{
+    delegate void EventHandler(string commandType, string direction, int quantity);
+    class Lift : EventArgs
+    {
+        public static event EventHandler LiftEvents;
+
+        public Lift()
+        { }
+
+        public void LiftUp(string commandType, string direction, int quantity)
+        {
+            if (commandType.Equals("Move"))
+            {
+                if (direction.Equals("LiftUp"))
+                    Console.WriteLine("Lift is up " + quantity);
+            }
+        }
+        public void LiftDown(string commandType, string direction, int quantity)
+        {
+            if (commandType.Equals("Move"))
+            {
+                if (direction.Equals("LiftDown"))
+                    Console.WriteLine("Lift is down " + quantity);
+            }
+        }
+        public void LiftForward(string commandType, string direction, int quantity)
+        {
+            if (commandType.Equals("Move"))
+            {
+                if (direction.Equals("LiftForward"))
+                    Console.WriteLine("Lift is forward " + quantity);
+            }
+        }
+        public void LiftBackward(string commandType, string direction, int quantity)
+        {
+            if (commandType.Equals("Move"))
+            {
+                if (direction.Equals("LiftBackward"))
+                    Console.WriteLine("Lift is backward " + quantity);
+            }
+        }
+        public void LiftStop(string commandType, string direction, int quantity)
+        {
+            if (commandType.Equals("Stop"))
+            {
+                Console.WriteLine("Lift is stopped ");
+            }
+        }
+
+        public void AddLiftMovings()
+        {
+            //LiftEvents += LiftUp;
+            LiftEvents += LiftDown;
+            LiftEvents += LiftForward;
+            //LiftEvents += LiftBackward;
+            LiftEvents += LiftStop;
+
+            Console.WriteLine("Movings added ");
+        }
+
+        public void ExecuteLiftEventsAccordingToInput(string commandType, string direction, int quantity)
+        {
+            LiftEvents(commandType, direction, quantity);
+        }
+    }
+}
+
+
---
+++ EventMechanism/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EventMechanism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lift lift = new Lift();
+
+            lift.AddLiftMovings();
+
+            lift.ExecuteLiftEventsAccordingToInput("Move", "LiftUp", 5); //Lift is not in LiftEvents Lift.cs Line 59
+            lift.ExecuteLiftEventsAccordingToInput("Move", "LiftDown", 1); //Lift Down is in LiftEvents Lift.cs Line 60
+            lift.ExecuteLiftEventsAccordingToInput("Move", "LiftForward", 45); //LiftForward is in LiftEvents Lift.cs Line 61
+            lift.ExecuteLiftEventsAccordingToInput("Move", "LiftBackward", 55); //LiftBackward is not in LiftEvents Lift.cs Line 62
+            lift.ExecuteLiftEventsAccordingToInput("Stop", "LiftStop", 5); //LiftBackward is in LiftEvents Lift.cs Line 63
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
